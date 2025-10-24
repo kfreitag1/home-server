b198e2624f883f816767bcaff6ba61d6d50c6f6f
Kieran Freitag <kfreitag@kieran.ca>
2025-10-23 23:11:12 -0700

    rekey secrets for new server pt2

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,5 +1,5 @@
 let
-  server = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEG2TpITPEazCINcPbeY6zndBWAoZRNSfRD9rhWI96iu kieran@nixos";
+  server = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAcM3fGjT0HJ2ApA0m/6oyFJV9HBJk/9JzhB3P0IVdOu root@homeserver";
   laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICQzXCHZhk/rHUSLI8+E5lNM3O1ZoZWUjyOPZG7Ivb5u kieran@Mac-2.lan";
   allKeys = [ server laptop ];
 in
