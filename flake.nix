22202269799a7d1c14ad61c370b558eb6d8233fe
Kieran Freitag <kfreitag@kieran.ca>
2026-04-21 19:54:57 -0700

    feat: add openssh MACs for Echo iOS client

--- flake.nix
+++ flake.nix
@@ -195,7 +195,16 @@
         # Services
         services.openssh = {
           enable = true;
-          settings.PasswordAuthentication = false;
+          settings = {
+            PasswordAuthentication = false;
+            Macs = [
+              "hmac-sha2-512-etm@openssh.com"
+              "hmac-sha2-256-etm@openssh.com"
+              "umac-128-etm@openssh.com"
+              "hmac-sha2-512"
+              "hmac-sha2-256"
+            ];
+          };
         };
 
         services.home-assistant = {
